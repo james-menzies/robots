8e11a0b77985ac757403f67ccfd2c4f3070899c1
James Menzies <james.r.menzies@gmail.com>
2021-09-30 12:05:12 +1000

    test passing for Coordinate

--- app/src/main/java/robots/models/Coordinate.java
+++ app/src/main/java/robots/models/Coordinate.java
@@ -6,6 +6,11 @@ public class Coordinate {
     private int y;
 
     public Coordinate(int x, int y) {
+
+        // Enforce positive indices only
+        if (x < 0 || y < 0) {
+            throw new IllegalStateException("Must not supply negative indices to Coordinate class.");
+        }
         this.x = x;
         this.y = y;
     }
@@ -16,5 +21,17 @@ public class Coordinate {
 
     public int getY() {
         return y;
+    }
+
+    // Provide equality based on x and y attributes
+    @Override
+    public boolean equals(Object obj) {
+       if (!(obj instanceof Coordinate)) {
+           return false;
+       }
+
+       Coordinate other = (Coordinate) obj;
+
+       return this.x == other.getX() && this.y == other.getY();
     }
 }
