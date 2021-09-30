3c99b771dea02b57e0aad517cc3fc42bcf934630
James Menzies <james.r.menzies@gmail.com>
2021-09-30 11:58:11 +1000

    tests written for Coordinate

--- app/src/test/java/robots/models/CoordinateTest.java
+++ app/src/test/java/robots/models/CoordinateTest.java
@@ -1,8 +1,27 @@
 package robots.models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CoordinateTest {
 
+    @Test
+    public void testEqualsFunctionality() {
+        Coordinate c1 = new Coordinate(2, 3);
+        Coordinate c2 = new Coordinate(2, 3);
 
+        assertEquals("Coordinates with equal x and y properties should be equal",
+                c1, c2);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testIllegalXAttribute() {
+        new Coordinate(-1, 2);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testIllegalYAttribute() {
+        new Coordinate(1, -2);
+    }
 }
