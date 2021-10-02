9a25089ac905c2452204cea6b561b7d3df467190
James Menzies <james.r.menzies@gmail.com>
2021-10-02 12:45:44 +1000

    dispatch logic written and passing.

--- app/src/main/java/robots/views/Display.java
+++ app/src/main/java/robots/views/Display.java
@@ -2,10 +2,22 @@ package robots.views;
 
 import robots.controllers.IController;
 import robots.controllers.RobotDescription;
+import robots.models.Coordinate;
+import robots.models.Orientation;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 public class Display {
+    /*
+    Any errors that emerge from parsing user input will be ignored. Whilst not
+    best practice, there are no guidelines in the description as to how this
+    situation should be handled.
+
+    If users were meant to receive feedback on invalid input, it would be contained
+    within this class.
+     */
 
     private static IController controller;
 
@@ -25,6 +37,71 @@ public class Display {
 
     static void dispatch(String command) {
 
+        if (command.length() == 0) {
+            return;
+        }
+
+        String[] tokens = command.split(" ");
+        String operation = tokens[0].toUpperCase(Locale.ROOT);
+        String argument = tokens.length > 1 ? tokens[1] : null;
+
+        switch (operation) {
+            case "LEFT":
+                controller.onLeft();
+                break;
+            case "RIGHT":
+                controller.onRight();
+                break;
+            case "REPORT":
+                controller.onReport();
+                break;
+            case "MOVE":
+                controller.onMove();
+                break;
+            case "PLACE":
+                Display.processOnPlace(argument);
+                break;
+            case "ROBOT":
+                Display.processOnRobot(argument);
+                break;
+            default:
+                return;
+        }
+
+
+    }
+
+    private static void processOnPlace(String argument) {
+
+        if (Objects.isNull(argument)) {
+            return;
+        }
+
+        String [] tokens = argument.split(",");
+
+        if (tokens.length != 3) {
+            return;
+        }
+
+        try {
+            int x = Integer.parseInt(tokens[0]);
+            int y = Integer.parseInt(tokens[1]);
+            Coordinate coordinate = new Coordinate(x, y);
+            Orientation orientation = Orientation.valueOf(tokens[2]);
+            controller.onPlace(coordinate, orientation);
+        } catch (IllegalArgumentException e) {
+            return;
+        }
+    }
+
+    private static void processOnRobot(String argument) {
+        try {
+            int reference = Integer.parseInt(argument);
+            controller.onRobot(reference);
+
+        } catch (NumberFormatException e) {
+            return;
+        }
     }
 
 }
