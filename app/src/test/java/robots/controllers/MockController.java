27b67f8f58037a2a8b096a7398c260a1b55ccaf4
James Menzies <james.r.menzies@gmail.com>
2021-10-02 12:19:46 +1000

    display tests written

--- app/src/test/java/robots/controllers/MockController.java
+++ app/src/test/java/robots/controllers/MockController.java
@@ -7,14 +7,14 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class MockController implements IController {
+    /* The intent of this class is to provide a mock interface for the
+        Display class to interact with in unit testing. Its job is to
+        simply keep track of the last handle that was called.
+     */
 
 
     private String lastHandleCalled;
 
-    public MockController() {
-
-        this.lastHandleCalled = "";
-    }
     @Override
     public void onPlace(Coordinate c, Orientation orientation) {
         this.lastHandleCalled = "onPlace";
--- app/src/test/java/robots/views/DisplayTest.java
+++ app/src/test/java/robots/views/DisplayTest.java
@@ -2,6 +2,7 @@ package robots.views;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import robots.controllers.MockController;
 
 import static org.junit.Assert.*;
@@ -17,11 +18,64 @@ public class DisplayTest {
         Display.setController(controller);
     }
 
+    @Test
+    public void zeroArgumentCommandsAreHandled() {
+        // Verify that 'left', 'right', 'move' and 'report' all get called.
+
+        Display.dispatch("LEFT");
+        assertEquals("onLeft", controller.getLastHandleCalled());
+
+        Display.dispatch("RIGHT");
+        assertEquals("onRight", controller.getLastHandleCalled());
+
+        Display.dispatch("MOVE");
+        assertEquals("onMove", controller.getLastHandleCalled());
+
+        Display.dispatch("REPORT");
+        assertEquals("onReport", controller.getLastHandleCalled());
+    }
+
+    @Test
+    public void invalidCommandIsIgnored() {
+
+        Display.dispatch("INVALID");
+        assertNull(controller.getLastHandleCalled());
+    }
+
+    @Test
+    public void validRobotCommandIsHandled() {
+        Display.dispatch("ROBOT 2");
+        assertEquals("onRobot", controller.getLastHandleCalled());
+    }
+
+    @Test
+    public void invalidRobotCommandIsIgnored() {
+        Display.dispatch("ROBOT TWO");
+        assertNull(controller.getLastHandleCalled());
+    }
+
+    @Test
+    public void validPlaceCommandIsHandled() {
+        Display.dispatch("PLACE 3,3,NORTH");
+        assertEquals("onPlace", controller.getLastHandleCalled());
+    }
+
+    @Test
+    public void invalidPlaceCommandIsIgnored() {
+        Display.dispatch("PLACE 2,ONE,NORTHWEST");
+    }
+
+    @Test
+    public void commandsWithArgumentsMustHaveArguments() {
+        Display.dispatch("PLACE");
+        assertNull(controller.getLastHandleCalled());
+
+        Display.dispatch("ROBOT");
+        assertNull(controller.getLastHandleCalled());
+    }
 
     @After
     public void after() {
         controller = null;
     }
-
-
 }
