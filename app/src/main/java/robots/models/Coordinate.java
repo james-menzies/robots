4219d7279cdc5f4bd8c7bd945ec0e017f0755ab5
James Menzies <james.r.menzies@gmail.com>
2021-10-04 13:37:06 +1100

    variables made final in Coordinate

--- app/src/main/java/robots/models/Coordinate.java
+++ app/src/main/java/robots/models/Coordinate.java
@@ -4,8 +4,8 @@ import java.util.Objects;
 
 public class Coordinate {
 
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
 
     public Coordinate(int x, int y) {
 
